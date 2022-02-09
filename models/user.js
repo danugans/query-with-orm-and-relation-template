f1d83c3504c996ce5e8107de2aaaafd51aa83134
danugans <mas.danu29@gmail.com>
2022-02-09 12:30:44 +0700

    update

--- models/user.js
+++ models/user.js
@@ -17,6 +17,26 @@ module.exports = (sequelize, DataTypes) => {
       });
 
       // code here
+      user.hasMany(models.product, {
+        as: "products",
+        foreignKey: {
+          name: "idUser",
+        },
+      });
+
+      user.hasMany(models.transaction, {
+        as: "buyerTransaction",
+        foreignKey: {
+          name: "idBuyer",
+        },
+      });
+
+      user.hasMany(models.transaction, {
+        as: "sellerTransaction",
+        foreignKey: {
+          name: "idSeller",
+        },
+      });
     }
   }
   user.init(
